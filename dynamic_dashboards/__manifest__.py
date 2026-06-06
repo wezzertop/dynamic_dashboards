54a8cea65f1eaf5388a4143c190200dc3c7cdaa1
Daniel <wezzertop@gmail.com>
2026-06-06 14:25:30 -0600

    Update module price to 15.00 USD

--- dynamic_dashboards/__manifest__.py
+++ dynamic_dashboards/__manifest__.py
@@ -39,7 +39,7 @@ Key Features:
         ],
     },
     'images': ['static/description/banner.gif', 'static/description/icon.png'],
-    'price': 40.00,
+    'price': 15.00,
     'currency': 'USD',
     'installable': True,
     'application': True,
